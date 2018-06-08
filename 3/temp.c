7ae9b3dd84b6f9ae8378f9dafe1e0712c80e2379
gao-lex <1105856206@qq.com>
2018-06-08 12:45:06 +0800

    doing 3

--- 3/temp.c
+++ 3/temp.c
@@ -1,21 +1,6 @@
-long switch_eg (long x, long y, long z){
-	long w = 1;
-	switch (x) {
-		case 1:
-			w = y*z;
-			break;
-		case 2:
-			w = y/z;
-			// fall through
-		case 3:
-			w += z;
-			break;
-		case 5:
-		case 6:
-			w -= z;
-			break;
-		default:
-			w = 2;
-	}
-	return w;
+#include <stdio.h>
+int main(){
+	long local;
+	printf("loacl at %p\n",&local);
+	return 0;
 }
